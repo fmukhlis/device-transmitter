053e13b9b5423f9d12d23e1d4a1c0329fe4ed21b
fmukhlis <cipherporu9@gmail.com>
2023-04-23 00:13:45 +0700

    First Commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import {Link,Head} from '@inertiajs/react'
+
+const Home = (props) => {
+    return (
+        <>
+            <Head title="Dashboard" />
+            <h1>Device Transmitter</h1>
+        </>
+    )
+}
+
+export default Home
---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title inertia>Device Transmitter</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+    @inertiaHead
+</head>
+
+<body class='font-sans'>
+    @inertia
+</body>
+
+</html>
